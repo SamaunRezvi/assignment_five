c12492d3065c1d7508a6d032063f92e1c7bef740
Samaun <samaunrezvi9@gmail.com>
2026-09-13 20:55:49 +0600

    add base styles, brand gradient and app entry point

---
+++ src/App.jsx
@@ -0,0 +1,20 @@
+import { ToastContainer } from 'react-toastify'
+import 'react-toastify/dist/ReactToastify.css'
+import Navbar from './components/Navbar'
+import Hero from './components/Hero'
+import TechnologiesSection from './components/TechnologiesSection'
+import Footer from './components/Footer'
+
+function App() {
+  return (
+    <div className="min-h-screen bg-white">
+      <Navbar />
+      <Hero />
+      <TechnologiesSection />
+      <Footer />
+      <ToastContainer position="top-right" autoClose={2500} />
+    </div>
+  )
+}
+
+export default App
